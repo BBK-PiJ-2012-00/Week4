4e444e08308a787aa904bf152e7f9c4c9a298e19
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2012-10-30 12:01:27 +0000

    Remove needs to be added.  Otherwise working.

--- Map.java
+++ Map.java
@@ -1,52 +1,52 @@
-public class Map implements SimpleMap { //you either have a list inside map, or an array
-	private int key;
-	private String value;
+public class Map { //you either have a list inside map, or an array
 	private KeyStore head = null;
 	private KeyStore tail = null;
-	private KeyList; //this is what your map is!
-	private KeyStore traverse = null;
-	
-	 
+	private KeyStore traverse = null;	 
 	
 /**
 * Puts a new String in the map.  If the key is already in the map, nothing is done.
 */
-	public String put(int key, String name) { //compare keys here, then addToList if successful
+	public String put(int key, String name) { //WORKING
 		if (isEmpty() == false) {
-			if (containsKey(head, key) == false) {
-				if(head.getNext().getKey() == null) { //need to create a next first....
-					head.getNext().setKey(key);
-					head.getNext().setName(name);
-					return head.getNext().getName();
+			if (containsKey(key) == false) {
+				createItem(key, name);
 			}
-			else {
-				d
-			
-		
-		
-		
-		
-		
-		if (head.getKey() == null) {			
-			head.setName(name);
-			head.setKey(key);
-			tail = head;
-			KeyStore.incrementKeyCount();
-			return head.getName();
+			else if (containsKey(key) == true) {
+				String duplicateAlert = "This ID already exists.";
+				return duplicateAlert;
+			}
 		}
-		else if (head.getKey() != null) {
-			int counter;
-			traverse = head;
-			
-				
-			// replace with while loop, breaking as soon as matching key is found, and making new key tail if no match found
-				
+		else if (isEmpty() == true) {
+			createItem(key, name);
+		}
+		return name;
 	}
 	
-	public boolean containsKey(KeyStore head, int key) { //checks keys
+/**
+* Returns the name associated with that key, or null if there is none.
+*/
+	String get(int key) { //WORKING
+		traverse = head;
+		int i;
+		for (i = 0; i < KeyStore.getKeyCount(); i++) {
+			if (traverse.getKey() == key) {
+				return traverse.getName();
+			}
+			else {
+				traverse = traverse.getNext();
+			}
+		}
+		String notFound = "ID not found.";
+		return notFound;	
+	}		
+	
+	
+	public boolean containsKey(int key) { //WORKING
 			traverse = head;
-			int counter;
-			for (counter = 0; counter < KeyStore.getKeyCount(); counter++) {
+			int i;
+			System.out.println(traverse.getKey());
+			System.out.println(KeyStore.getKeyCount());
+			for (i = 0; i < KeyStore.getKeyCount(); i++) {
 				if (traverse.getKey() == key) {
 					return true;
 				}
@@ -61,25 +61,22 @@ public class Map implements SimpleMap { //you either have a list inside map, or 
 		if (head == null) {
 			return true;
 		}
+		return false;
 	}
 	
-	public void addToList(KeyStore newItem) {
+	public void createItem(int key, String name) { //WORKING
+		KeyStore newItem = new KeyStore(key, name);
+		if (isEmpty()) {			
+			head = newItem;
+			tail = newItem;
+		}
+		else {
 			tail.setNext(newItem);
 			tail = newItem;
-	}
+		}
+	}	
+	
 		
-					
-	
-	/**
-* Returns the name associated with that key, or null if there is none.
-*/
-	String get(int key) {
-	
-	}
-	
-	
-	
-	
 	
 	
 	
@@ -92,41 +89,33 @@ public class Map implements SimpleMap { //you either have a list inside map, or 
 	
 	private void launch() {
 	
-	KeyStore item1 = new KeyStore("Reno", 2197);
-	head = item1; //setting the head here means there is always a head -- can change this so that head is created
-	tail = item1;
-	String mappedName = put(item1.getKey(), item1.getName());
-	System.out.println(mappedName);
+	int i;
+	for (i = 0; i < 6; i++) {
+	System.out.println("Please enter the name and ID of the employee: ");
+	String name = System.console().readLine();
+	int id = Integer.parseInt(System.console().readLine());
+	String mappedName = put(id, name);
+	System.out.println(mappedName + " has been added to the database.");
+	}
 	
-	KeyStore item2 = new KeyStore();
-	addToList(item2);
+	System.out.println("Enter an ID number to obtain the employee's name: ");
+	int id = Integer.parseInt(System.console().readLine());
+	String requestedID = get(id);
+	System.out.println("The employee associated with that ID is: " + requestedID);
 	
-	KeyStore item3 = new KeyStore();
-	addToList(item3);
+
+	}
+
+}
 	
-	KeyStore item4 = new KeyStore();
-	addToList(item4);
+	//Only Remove to add
 	
 	
+	//Can fix confusing println about 'has been added' so that when someone isn't added
+	//because a duplicate exists, it's clear that no one's been added.  Minor point.
 	
 	
-	String name2 = "Tseng";
-	int idTseng = 1999;
-	//KeyStore key2 = new KeyStore("Tseng", 1999); 
-	String mappedName2 = put(1999, "Tseng");
-	System.out.println(mappedName2);
-	
-	String name3 = "Elena";
-	int idElena = 7623;
-	
-	String name4 = "Rude";
-	int idRude = 2197;
-	
-	//you want to test that the key doesn't appear in the map before you can assign it a value and add it to the list, 
-	//so you take a value and a key, into put(), you check that the key doesn't appear, and you then assign it to an item in the
-	//list - the next one.  So you should have a linked list of empty items, other than the head, to which things are assigned
-	// so you already have a linked list which constitutes your map, and you want to add keys and values to it
-	// so link your items as a list first, and then you see what you can put in it.
-	
+	//if isEmpty is true, you create the box and put the name and key in it, which becomes the head (and tail).  
+	//otherwise, you check the key against the list and if it's unique, you create a box, put the values in, and link it
 	
 	
