f4a9dbd1c30f3e2ff97e5d4177eaf4755018c767
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2012-11-14 12:05:01 +0000

    Complete.

--- IntegerTreeNode.java
+++ IntegerTreeNode.java
@@ -2,8 +2,7 @@ public class IntegerTreeNode {
 	private int value;
 	private IntegerTreeNode left = null;
 	private IntegerTreeNode right = null;
-	private IntegerTreeNode traverse;
-	
+		
 	public IntegerTreeNode(int n) {
 		this.value = n;	
 	}
@@ -12,12 +11,12 @@ public class IntegerTreeNode {
 		return this.value;
 	}
 	
-	public int getLeft() {
-		return this.left.value;
+	public IntegerTreeNode getLeft() {
+		return this.left;
 	}
 	
-	public int getRight() {
-		return this.right.value;
+	public IntegerTreeNode getRight() {
+		return this.right;
 	}
 	
 	public void add(int newNumber) {
@@ -53,7 +52,6 @@ public class IntegerTreeNode {
 			return false;
 		}
 	}
-
 	
 	public int getMin() { //returns smallest number in tree
 		if (left != null) {
@@ -69,10 +67,83 @@ public class IntegerTreeNode {
 		return this.value;
 	}
 	
-}
+	public String toString() {
+		String string = "";
+		string = string + "[" + this.value + " ";		
+		if (this.left == null) {
+			string = string + "L[] ";
+		}
+		else { 
+			string = string + "L ";
+			string = string + left.toString();
+		}
+		if (this.right == null) {
+			string = string + "R[] ";
+		}
+		else {
+			string = string +  "R " + right.toString();
+		}
+		return string + "]";
+	}
 	
+	public String toSimpleString() {
+		String string = "";
+		string = string + "[" + this.value + " ";
+		if (this.left != null) {
+			string = string + left.toSimpleString();
+		}
+		if (this.right != null) {
+			string = string + right.toSimpleString();
+		}
+		return string + "]";
+	}
+	
+	public int depth() { //find the deepest subtree
+		int leftDepth = 0;
+		int rightDepth = 0;
+		if (this.left != null) {
+			leftDepth = left.depth();
+		} 
+		if (this.right != null) {
+			rightDepth = right.depth();
+		}
+		if (rightDepth > leftDepth) {
+			return rightDepth + 1;
+		}
+		else {
+			return leftDepth + 1;
+		}
+	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 		
+			
+			
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+}
 
-	
-	
+/**
+Add a method depth() that returns the number of levels in a tree. By convention, a tree with only one node
+(i.e. the root) has a depth of zero. Hint: the depths of the trees in Figure 1 are 0, 1, 2, and 3.
+Hint: the depth of a tree is one more than the deepest of its subtreees.
+*/
